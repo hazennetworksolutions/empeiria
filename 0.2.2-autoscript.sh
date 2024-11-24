4b009b88d16dbf6d9515d2270e4a0ff7a47b0471
hazennetworksolutions <161881320+hazennetworksolutions@users.noreply.github.com>
2024-11-24 16:34:50 +0300

    Create 0.2.2-autoscript.sh

---
+++ 0.2.2-autoscript.sh
@@ -0,0 +1,245 @@
+#!/bin/bash
+LOG_FILE="/var/log/empeiria_node_install.log"
+exec > >(tee -a "$LOG_FILE") 2>&1
+
+printGreen() {
+    echo -e "\033[32m$1\033[0m"
+}
+
+printLine() {
+    echo "------------------------------"
+}
+
+# Function to print the node logo
+function printNodeLogo {
+    echo -e "\033[32m"
+    echo "          
+████████████████████████████████████████████████████████████████████████████████████████████████████
+████████████████████████████████████████████████████████████████████████████████████████████████████
+████████████████████████████████████████████████████████████████████████████████████████████████████
+████████████████████████████████████████████████████████████████████████████████████████████████████
+████████████████████████████████████████████████████████████████████████████████████████████████████
+████████████████████████████████████████████████████████████████████████████████████████████████████
+████████████████████████████████████████████████████████████████████████████████████████████████████
+████████████████████████████████████████████████████████████████████████████████████████████████████
+████████████████████████████████████████████████████████████████████████████████████████████████████
+████████████████████████████████████████████████████████████████████████████████████████████████████
+████████████████████████████████████████████████████████████████████████████████████████████████████
+████████████████████████████████████████████████████████████████████████████████████████████████████
+████████████████████████████████████████████████████████████████████████████████████████████████████
+████████████████████████████████████████████████████████████████████████████████████████████████████
+██████████████████████████████████████████████        ██████████████████████████████████████████████
+███████████████████████████████████████████              ███████████████████████████████████████████
+████████████████████████████████████████                    ████████████████████████████████████████
+█████████████████████████████████████                          █████████████████████████████████████
+█████████████████████████████████                                  █████████████████████████████████
+██████████████████████████████             █             █            ██████████████████████████████
+████████████████████████████           █████             ████           ████████████████████████████
+████████████████████████████          ██████             ██████         ████████████████████████████
+████████████████████████████          ██████             ██████          ███████████████████████████
+████████████████████████████          ███████            ██████          ███████████████████████████
+████████████████████████████          ██████████         ██████          ███████████████████████████
+████████████████████████████          █████████████      ██████          ███████████████████████████
+████████████████████████████             █████████████     ████          ███████████████████████████
+████████████████████████████          █     █████████████     █          ███████████████████████████
+████████████████████████████          █████     ████████████             ███████████████████████████
+████████████████████████████          ██████       ████████████          ███████████████████████████
+████████████████████████████          ██████          █████████          ███████████████████████████
+████████████████████████████          ██████             ██████          ███████████████████████████
+████████████████████████████          ██████             ██████          ███████████████████████████
+████████████████████████████          ██████             ██████         ████████████████████████████
+████████████████████████████            ████             ███            ████████████████████████████
+██████████████████████████████                                        ██████████████████████████████
+█████████████████████████████████                                  █████████████████████████████████
+█████████████████████████████████████                           ████████████████████████████████████
+████████████████████████████████████████                    ████████████████████████████████████████
+███████████████████████████████████████████              ███████████████████████████████████████████
+██████████████████████████████████████████████        ██████████████████████████████████████████████
+████████████████████████████████████████████████████████████████████████████████████████████████████
+████████████████████████████████████████████████████████████████████████████████████████████████████
+████████████████████████████████████████████████████████████████████████████████████████████████████
+████████████████████████████████████████████████████████████████████████████████████████████████████
+████████████████████████████████████████████████████████████████████████████████████████████████████
+████████████████████████████████████████████████████████████████████████████████████████████████████
+████████████████████████████████████████████████████████████████████████████████████████████████████
+████████████████████████████████████████████████████████████████████████████████████████████████████
+████████████████████████████████████████████████████████████████████████████████████████████████████
+████████████████████████████████████████████████████████████████████████████████████████████████████
+████████████████████████████████████████████████████████████████████████████████████████████████████
+████████████████████████████████████████████████████████████████████████████████████████████████████
+████████████████████████████████████████████████████████████████████████████████████████████████████
+████████████████████████████████████████████████████████████████████████████████████████████████████
+Hazen Network Solutions 2024 All rights reserved."
+    echo -e "\033[0m"
+}
+
+# Show the node logo
+printNodeLogo
+
+# User confirmation to proceed
+echo -n "Type 'yes' to start the installation Empeiria v0.2.2 and press Enter: "
+read user_input
+
+if [[ "$user_input" != "yes" ]]; then
+  echo "Installation cancelled."
+  exit 1
+fi
+
+# Function to print in green
+printGreen() {
+  echo -e "\033[32m$1\033[0m"
+}
+
+printGreen "Starting installation..."
+sleep 1
+
+printGreen "If there are any, clean up the previous installation files"
+
+sudo systemctl stop emped
+sudo systemctl disable emped
+sudo rm -rf /etc/systemd/system/emped.service
+sudo rm $(which emped)
+sudo rm -rf $HOME/.empe
+sed -i "/emped_/d" $HOME/.bash_profile
+
+# Update packages and install dependencies
+printGreen "1. Updating and installing dependencies..."
+sudo apt-get update && sudo apt-get upgrade -y
+sudo apt-get install curl git wget htop tmux build-essential jq make lz4 gcc unzip -y
+
+# User inputs
+read -p "Enter your MONIKER: " MONIKER
+echo 'export MONIKER='$MONIKER
+read -p "Enter your PORT (2-digit): " PORT
+echo 'export PORT='$PORT
+
+# Setting environment variables
+echo "export MONIKER=$MONIKER" >> $HOME/.bash_profile
+echo "export EMPED_CHAIN_ID=\"empe-testnet-2\"" >> $HOME/.bash_profile
+echo "export EMPED_PORT=$PORT" >> $HOME/.bash_profile
+source $HOME/.bash_profile
+
+printLine
+echo -e "Moniker:        \e[1m\e[32m$MONIKER\e[0m"
+echo -e "Chain ID:       \e[1m\e[32m$EMPED_CHAIN_ID\e[0m"
+echo -e "Node custom port:  \e[1m\e[32m$EMPED_PORT\e[0m"
+printLine
+sleep 1
+
+# Install Go
+printGreen "2. Installing Go..." && sleep 1
+cd $HOME
+VER="1.23.0"
+wget "https://golang.org/dl/go$VER.linux-amd64.tar.gz"
+sudo rm -rf /usr/local/go
+sudo tar -C /usr/local -xzf "go$VER.linux-amd64.tar.gz"
+rm "go$VER.linux-amd64.tar.gz"
+[ ! -f ~/.bash_profile ] && touch ~/.bash_profile
+echo "export PATH=\$PATH:/usr/local/go/bin:~/go/bin" >> ~/.bash_profile
+source $HOME/.bash_profile
+[ ! -d ~/go/bin ] && mkdir -p ~/go/bin
+
+# Version check
+echo $(go version) && sleep 1
+
+# Download Prysm protocol binary
+printGreen "3. Downloading Empeiria binary and setting up..." && sleep 1
+wget https://github.com/empe-io/empe-chain-releases/raw/master/v0.2.2/emped_v0.2.2_linux_amd64.tar.gz
+tar -xvf emped_v0.2.2_linux_amd64.tar.gz
+rm -rf emped_v0.2.2_linux_amd64.tar.gz
+chmod +x emped
+mkdir -p $HOME/.empe-chain/cosmovisor/genesis/bin
+mv $HOME/emped $HOME/.empe-chain/cosmovisor/genesis/bin
+sudo ln -sf $HOME/.empe-chain/cosmovisor/genesis $HOME/.empe-chain/cosmovisor/current -f
+sudo ln -sf $HOME/.empe-chain/cosmovisor/current/bin/emped /usr/local/bin/emped -f
+
+# Create service file
+printGreen "6. Creating service file..." && sleep 1
+sudo tee /etc/systemd/system/emped.service > /dev/null << EOF
+[Unit]
+Description=empe-chain node service
+After=network-online.target
+
+[Service]
+User=$USER
+ExecStart=$(which cosmovisor) run start
+Restart=on-failure
+RestartSec=10
+LimitNOFILE=65535
+Environment="DAEMON_HOME=${HOME}/.empe-chain"
+Environment="DAEMON_NAME=emped"
+Environment="UNSAFE_SKIP_BACKUP=true"
+Environment="PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin:$HOME/.emped/cosmovisor/current/bin"
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+
+# Enable the service
+sudo systemctl daemon-reload
+sudo systemctl enable emped
+
+# Initialize the node
+printGreen "7. Initializing the node..."
+emped config set client chain-id ${EMPED_CHAIN_ID}
+emped config set client keyring-backend test
+emped config set client node tcp://localhost:${EMPED_PORT}657
+emped init ${MONIKER} --chain-id ${EMPED_CHAIN_ID}
+
+# Download genesis and addrbook files
+printGreen "8. Downloading genesis and addrbook..."
+wget -O $HOME/.empe-chain/config/genesis.json https://raw.githubusercontent.com/hazennetworksolutions/empeiria/refs/heads/main/genesis.json
+wget -O $HOME/.empe-chain/config/addrbook.json  https://raw.githubusercontent.com/hazennetworksolutions/empeiria/refs/heads/main/addrbook.json
+
+# Configure gas prices and ports
+printGreen "9. Configuring custom ports and gas prices..." && sleep 1
+sed -i 's|minimum-gas-prices =.*|minimum-gas-prices = "0.0001uempe"|g' $HOME/.empe-chain/config/app.toml
+sed -i.bak -e "s%:1317%:${EMPED_PORT}317%g;
+s%:8080%:${EMPED_PORT}080%g;
+s%:9090%:${EMPED_PORT}090%g;
+s%:9091%:${EMPED_PORT}091%g;
+s%:8545%:${EMPED_PORT}545%g;
+s%:8546%:${EMPED_PORT}546%g;
+s%:6065%:${EMPED_PORT}065%g" $HOME/.empe-chain/config/app.toml
+# Configure P2P and ports
+sed -i.bak -e "s%:26658%:${EMPED_PORT}658%g;
+s%:26657%:${EMPED_PORT}657%g;
+s%:6060%:${EMPED_PORT}060%g;
+s%:26656%:${EMPED_PORT}656%g;
+s%^external_address = \"\"%external_address = \"$(wget -qO- eth0.me):${EMPED_PORT}656\"%;
+s%:26660%:${EMPED_PORT}660%g" $HOME/.empe-chain/config/config.toml
+
+# Set up seeds and peers
+printGreen "10. Setting up peers and seeds..." && sleep 1
+SEEDS="20ca5fc4882e6f975ad02d106da8af9c4a5ac6de@empeiria-testnet-seed.itrocket.net:28656"
+PEERS="03aa072f917ed1b79a14ea2cc660bc3bac787e82@empeiria-testnet-peer.itrocket.net:28656,a9cf0ffdef421d1f4f4a3e1573800f4ee6529773@136.243.13.36:29056,cbe1bfc8ee1a15a5e32ba85e0944d17812b5b244@65.21.67.40:34656,78f766310a83b6670023169b93f01d140566db79@65.109.83.40:29056,66ac611ba87753e92f1e5d792a2b19d4c5080f32@188.40.73.112:22656,45bdc8628385d34afc271206ac629b07675cd614@65.21.202.124:25656,1a260d047dc84b3f2b13d1b6a9f4c6295a2110f5@135.181.136.105:11156,fb0a0beeb42902053b526e0f2dd572305d89a26c@65.109.84.22:26656,e62b549646fee135cf010bc10641f728aba7fbd0@65.108.234.158:26626,2987c6802f3a227f2e423ec4548ae4f1a96cba9e@116.203.94.181:26656,4cb79afab8ff3912518af0fe630575cbad6c798e@95.217.61.32:7756,e058f20874c7ddf7d8dc8a6200ff6c7ee66098ba@65.109.93.124:29056,2db322b41d26559476f929fda51bce06c3db8ba4@65.109.24.155:11256"
+sed -i -e "/^\[p2p\]/,/^\[/{s/^[[:space:]]*seeds *=.*/seeds = \"$SEEDS\"/}" \
+       -e "/^\[p2p\]/,/^\[/{s/^[[:space:]]*persistent_peers *=.*/persistent_peers = \"$PEERS\"/}" $HOME/.empe-chain/config/config.toml
+
+# Pruning Settings
+sed -i -e "s/^pruning *=.*/pruning = \"custom\"/" $HOME/.empe-chain/config/app.toml 
+sed -i -e "s/^pruning-keep-recent *=.*/pruning-keep-recent = \"100\"/" $HOME/.empe-chain/config/app.toml
+sed -i -e "s/^pruning-interval *=.*/pruning-interval = \"19\"/" $HOME/.empe-chain/config/app.toml
+
+# Download the snapshot
+# printGreen "12. Downloading snapshot and starting node..." && sleep 1
+
+
+
+
+
+# Start the node
+printGreen "13. Starting the node..."
+sudo systemctl start emped
+
+# Check node status
+printGreen "14. Checking node status..."
+sudo journalctl -u emped -f -o cat
+
+# Verify if the node is running
+if systemctl is-active --quiet emped; then
+  echo "The node is running successfully! Logs can be found at /var/log/empeiria_node_install.log"
+else
+  echo "The node failed to start. Logs can be found at /var/log/empeiria_node_install.log"
+fi
